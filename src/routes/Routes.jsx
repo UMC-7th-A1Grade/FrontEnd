b22e65e1537d5f67691cfdc44bb8298caff4246b
YeChan Kim <az20058@naver.com>
2025-01-10 21:17:33 +0900

    🎨 Design : 마이페이지 UI 제작

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,25 @@
-import { Routes as ReactRouters, Route } from "react-router-dom";
-import Layout from "../layout/Layout";
+import { Routes as ReactRouters, Route } from 'react-router-dom';
+import Layout from '../layout/Layout';
 
 //pages
-import HomePage from "../pages/HomePage";
+import HomePage from '../pages/HomePage';
+import MyPage from '../pages/MyPage';
 
 const Routes = () => {
   return (
     <ReactRouters>
-      <Route path="/" element={<Layout />}>
-        <Route path="" element={<HomePage />} />
+      <Route
+        path='/'
+        element={<Layout />}
+      >
+        <Route
+          path=''
+          element={<HomePage />}
+        />
+        <Route
+          path='mypage'
+          element={<MyPage />}
+        />
       </Route>
     </ReactRouters>
   );
