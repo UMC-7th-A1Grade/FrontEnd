09fb7d8c916049ed57f685d519e6e4242117f6b7
lanapi <songhj1119@gmail.com>
2025-01-11 15:09:14 +0900

    ✨ 로그인 화면 UI 구현 완료

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,12 +3,21 @@ import Layout from "../layout/Layout";
 
 //pages
 import HomePage from "../pages/HomePage";
+import Ranking from "../pages/Ranking/Ranking";
+import StoragePage from "../pages/StoragePage";
+import Login from "../pages/LoginPage/LoginPage";
+import Register from "../pages/RegisterPage/RegisterPage";
+
 
 const Routes = () => {
   return (
     <ReactRouters>
       <Route path="/" element={<Layout />}>
         <Route path="" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
+        <Route path="ranking" element={<Ranking />} />
+        <Route path="storage" element={<StoragePage />} />
       </Route>
     </ReactRouters>
   );
