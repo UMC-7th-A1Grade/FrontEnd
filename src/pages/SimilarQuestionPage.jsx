0f9e3a94fb7233febf4062ea3117256e14061260
sungyeon <tjddus0731@naver.com>
2025-02-02 00:55:44 +0900

    🎨 Design: 헤더 추가

--- src/pages/SimilarQuestionPage.jsx
+++ src/pages/SimilarQuestionPage.jsx
@@ -1,9 +1,11 @@
+import Header from '../components/global/Header';
 import SimilarQuestionSection from '../components/similarQuestionPage/SimilarQuestionImage';
 import ButtonSection from '../components/similarQuestionPage/ButtonSection';
 
 export default function SimilarQuestionPage() {
   return (
     <>
+      <Header />
       <SimilarQuestionSection />
       <ButtonSection />
     </>
