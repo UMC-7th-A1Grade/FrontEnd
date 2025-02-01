697f13186749f498ab4d9c7ca6f1ac2af06acfff
YeChan Kim <az20058@naver.com>
2025-02-02 02:49:51 +0900

    🔀 Merge : branch 'main' into feature/MyPage

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +11,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         ws: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
