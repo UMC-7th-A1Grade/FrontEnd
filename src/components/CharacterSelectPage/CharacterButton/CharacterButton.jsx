074ca16006373321b775938dff7b09b5501dc232
lanapi <songhj1119@gmail.com>
2025-02-04 09:36:34 +0900

    🐛 온보딩 api 오류 수정

--- src/components/CharacterSelectPage/CharacterButton/CharacterButton.jsx
+++ src/components/CharacterSelectPage/CharacterButton/CharacterButton.jsx
@@ -1,28 +1,78 @@
-// components/CharacterSelectPage/CharacterButton/CharacterButton.jsx
+// // components/CharacterSelectPage/CharacterButton/CharacterButton.jsx
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import CustomButton from '../../global/CustomButton';
+
+// const CharacterButton = ({ isSelected }) => {
+//  const navigate = useNavigate();
+
+//  const handleClick = () => {
+//    if (isSelected) {
+//      navigate('/');
+//    } else {
+//      navigate('/nickname');
+//    }
+//  };
+
+//  return (
+//    <CustomButton 
+//      size="big"
+//      color={isSelected ? "blue" : "gray"}
+//      type="filled"
+//      onClick={handleClick}
+//      text={isSelected ? "선택완료" : "뒤로가기"}
+//    />
+//  );
+// };
+
+// export default CharacterButton;
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import CustomButton from '../../global/CustomButton';
+import api from '../../../apis/axios';
 
-const CharacterButton = ({ isSelected }) => {
- const navigate = useNavigate();
+const CharacterButton = ({ isSelected, selectedCharacter }) => {
+  const navigate = useNavigate();
 
- const handleClick = () => {
-   if (isSelected) {
-     navigate('/');
-   } else {
-     navigate('/nickname');
-   }
- };
+  const handleClick = async () => {
+    if (!isSelected) {
+      navigate('/nickname');
+      return;
+    }
 
- return (
-   <CustomButton 
-     size="big"
-     color={isSelected ? "blue" : "gray"}
-     type="filled"
-     onClick={handleClick}
-     text={isSelected ? "선택완료" : "뒤로가기"}
-   />
- );
+    try {
+      const nickname = localStorage.getItem('tempNickname');
+      if (!nickname) {
+        console.error('닉네임 정보가 없습니다.');
+        navigate('/nickname');
+        return;
+      }
+
+      const response = await api.patch('/users', {
+        nickname,
+        characterId: selectedCharacter
+      });
+
+      if (response.status === 200) {
+        localStorage.removeItem('tempNickname');
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert(error.response?.data?.message || '오류가 발생했습니다.');
+    }
+  };
+
+  return (
+    <CustomButton
+      size="big"
+      color={isSelected ? "blue" : "gray"}
+      type="filled"
+      onClick={handleClick}
+      text={isSelected ? "선택완료" : "뒤로가기"}
+    />
+  );
 };
 
 export default CharacterButton;
