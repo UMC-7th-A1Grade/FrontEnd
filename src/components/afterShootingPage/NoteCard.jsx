ed1db97773b3964e5b4e6a7745102780f3cc98c4
sungyeon <tjddus0731@naver.com>
2025-02-06 05:01:21 +0900

    ♻️ Refactor : 답안 입력 공학용 계산기 추가

--- src/components/afterShootingPage/NoteCard.jsx
+++ src/components/afterShootingPage/NoteCard.jsx
@@ -1,12 +1,48 @@
+import { useState } from 'react';
 import styles from '../../styles/afterShootingPage/noteCard.module.css';
+import Calculator from '../global/Calculator';
+import { FaChevronDown } from 'react-icons/fa';
 
 export default function NoteCard({ memo, setMemo }) {
+  const [isCalculatorOpen, setIsCalculatorOpen] = useState(false);
+  const [answer, setAnswer] = useState(''); // 답 input 상태 추가
+
+  const toggleCalculator = () => {
+    setIsCalculatorOpen((prev) => !prev);
+  };
+
   return (
     <div className={styles.cardContainer}>
+      {/* 답 입력 필드 */}
       <div className={styles.header}>
-        <button className={styles.answerButton}>답</button>
+        <input
+          type='text'
+          className={styles.answerInput}
+          placeholder='답'
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        />
+
+        {/* 계산기 토글 버튼튼*/}
+        <button
+          className={`${styles.toggleButton} ${isCalculatorOpen ? styles.rotated : ''}`}
+          onClick={toggleCalculator}
+        >
+          <FaChevronDown className={styles.arrowIcon} />
+        </button>
       </div>
 
+      {/* 계산기 */}
+      <div className={`${styles.calculatorContainer} ${isCalculatorOpen ? styles.open : styles.closed}`}>
+        {isCalculatorOpen && (
+          <Calculator
+            input={answer}
+            setInput={setAnswer}
+          />
+        )}
+      </div>
+
+      {/* 메모 입력 필드 */}
       <div className={styles.inputContainer}>
         <textarea
           placeholder='내용을 입력해주세요'
